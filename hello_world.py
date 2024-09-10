bd65196b1f16a1cbcd4509fda1e34b51d26391ee
JaemSiKi62 <kim07078037058@gmail.com>
2024-09-10 12:29:57 +0900

    summary2
    SUMMARY

--- hello_world.py
+++ hello_world.py
@@ -1,9 +1,10 @@
-def print_hello():
-	print("Hello World!!")
+def add(x, y):
+	a = x + y
+	return a
 
 def print_bye():
-	print("Bye World!!")
+	print("Bye World")
 
 if __name__ == '__main__':
-	print_hello()
 	print_bye()
+	print( add(1, 2) )
